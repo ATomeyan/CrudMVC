8cbdb6acb68cbb07441446459c31cfff463b45c5
Artur Tomeyan <artur.tomeyan@ucom.am>
2025-08-25 15:01:12 +0400

    Initial

--- src/main/java/com/bdg/crudmvc/model/User.java
+++ src/main/java/com/bdg/crudmvc/model/User.java
@@ -1,15 +1,8 @@
 package com.bdg.crudmvc.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
-@AllArgsConstructor
 @Entity
-@Data
-@NoArgsConstructor
 @Table(name = "user")
 public class User {
 
@@ -23,4 +16,46 @@ public class User {
     private String l_Name;
     @Column(name = "email")
     private String email;
+
+    public User() {
+    }
+
+    public User(int id, String f_Name, String l_Name, String email) {
+        this.id = id;
+        this.f_Name = f_Name;
+        this.l_Name = l_Name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getF_Name() {
+        return f_Name;
+    }
+
+    public void setF_Name(String f_Name) {
+        this.f_Name = f_Name;
+    }
+
+    public String getL_Name() {
+        return l_Name;
+    }
+
+    public void setL_Name(String l_Name) {
+        this.l_Name = l_Name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
